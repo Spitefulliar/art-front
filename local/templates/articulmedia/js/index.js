210f8b2f2afd938c11c46f17bf473f94cb40c239
Dmitry Smirnov <pim10rentero@gmail.com>
2016-11-24 13:48:56 +0300

    npm i

---
+++ local/templates/articulmedia/js/index.js
@@ -0,0 +1,18 @@
+// reqiurements
+var jquery = window.$ = window.jQuery = require('jquery');
+var sprite = require('./sprite.js');
+
+// var TweenMax = window.TweenMax = require('gsap');
+// // require('gsap/src/minified/plugins/CSSRulePlugin.min.js');
+
+// // var jquery_validate = require('../libs/jquery-validate/jquery.validate.min.js');
+// // var jquery_univalidate = require('./uni-validate.js');
+// // require('./animations.js');
+
+// //angular modules
+// import appName from './app/app.js';
+
+
+$(document).ready(function() {
+    
+});
