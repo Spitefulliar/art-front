5546e2b4a9493b0e1d1444ff1671b999cb1ee286
Dmitry Smirnov <pim10rentero@gmail.com>
2016-11-25 12:05:31 +0300

    header

--- html/header.php
+++ html/header.php
@@ -1,9 +1,42 @@
 <?php include './head.php';?>
 
 <!-- header -->
-<header class="header">
+<header class="header container">
 
-  Header
+	<a href="#" class="header__logo">
+		<img src="../local/templates/articulmedia/img/icon/logo.png" alt="Articul">
+	</a>
+	<nav class="nav">
+		<ul class="nav__list">
+			<li class="nav__item">
+				<a href="" class="nav__link">КОМПАНИЯ</a>
+			</li>
+			<li class="nav__item">
+				<a href="" class="nav__link">Экспертиза</a>
+			</li>
+			<li class="nav__item">
+				<a href="" class="nav__link">Работы</a>
+			</li>
+			<li class="nav__item">
+				<a href="" class="nav__link">Достижения</a>
+			</li>
+			<li class="nav__item">
+				<a href="" class="nav__link">Новости</a>
+			</li>
+			<li class="nav__item">
+				<a href="" class="nav__link">Карьера</a>
+			</li>
+			<li class="nav__item">
+				<a href="" class="nav__link">Контакты</a>
+			</li>
+		</ul>
+	</nav>
+	<a href="" class="header__link">
+		<img src="../local/templates/articulmedia/img/icon/icon-с.png" alt="">
+	</a>
+	<a href="" class="header__link">
+		<img src="../local/templates/articulmedia/img/icon/icon-p.png" alt="">
+	</a>
 
 </header>
 <!-- eof header -->
--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <?php include './header.php';?>
-<section class="content">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nobis tempore vero, sapiente, modi cumque delectus, sed veritatis eligendi quibusdam facilis libero voluptatibus ad quaerat accusamus hic fugiat quod nam.</p>
+<section class="content container">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nobis tempore vero, sapiente, modi cumque delectus, sed veritatis eligendi quibusdam facilis libero voluptatibus ad quaerat accusamus hic fugiat quod nam. lorem*50</p>
 </section>
 <?php include './footer.php';?>
