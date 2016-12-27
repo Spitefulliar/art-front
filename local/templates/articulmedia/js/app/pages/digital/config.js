f7f128d84fe453290c31aaf569accae7766a21a7
Dmitry Smirnov <pim10rentero@gmail.com>
2016-12-27 13:06:27 +0300

    digital tmp add

---
+++ local/templates/articulmedia/js/app/pages/digital/config.js
@@ -0,0 +1,6 @@
+export default  {  
+  name: 'Digital',
+  title: 'Articul Media Group: digital, стратегии, креатив, SMM, мобильный маркетинг, создание и поддержка веб-проектов, мобильные приложения',
+  state: 'digital',
+  url: '/digital/',
+};
