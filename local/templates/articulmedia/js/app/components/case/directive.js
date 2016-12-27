273ccc4a4e8aca50422c688e1a8ac509de3494be
Natalia Chigryay <spiteful.liar@gmail.com>
2016-12-27 11:32:17 +0300

    upd scrollify installed not implemented

--- local/templates/articulmedia/js/app/components/case/directive.js
+++ local/templates/articulmedia/js/app/components/case/directive.js
@@ -2,12 +2,39 @@
 import moduleConfig from './config';
 const MODULE_NAME = moduleConfig.name;
 
-export default ['$rootScope','$http', '$timeout', '$window', '$state', '$log',
-  function ($rootScope, $http, $timeout, $window, $state, $log) {
+export default ['$rootScope','$http', '$timeout', '$window', '$state', '$log', '$mdMedia', 
+  function ($rootScope, $http, $timeout, $window, $state, $log, $mdMedia) {
     var linkFunction = function linkFunction($scope, $element, $attributes) {
       $scope.getCase().then(function(currentCase){
         $scope.case = currentCase;
         $log.debug('case',$scope.case);
+
+          // $.scrollify({
+          //   section : ".case-block",
+          //   // sectionName : "case-block",
+          //   updateHash: false,
+          //   interstitialSection : "",
+          //   easing: "easeOutExpo",
+          //   scrollSpeed: 1100,
+          //   offset : 0,
+          //   scrollbars: false,
+          //   standardScrollElements: "",
+          //   setHeights: true,
+          //   touchScroll: true,
+          //   overflowScroll: false,
+          //   before:function() {},
+          //   after:function() {},
+          //   afterResize:function() {},
+          //   afterRender:function() {}
+          // });
+
+          // $scope.$watch(function() { return $mdMedia('gt-sm'); }, function(mquery) {
+          //   if (mquery) {
+          //     $.scrollify.enable();
+          //   } else {
+          //     $.scrollify.disable();
+          //   }
+          // });
       });
     };
   return {
--- local/templates/articulmedia/js/vendor.js
+++ local/templates/articulmedia/js/vendor.js
@@ -2,6 +2,7 @@
 import 'babel-polyfill';
 import 'jquery';
 import 'slick-carousel';
+import 'jquery-scrollify';
 import 'hamsterjs';
 import '../libs/jQuery-Waterwheel-Carousel/js/jquery.waterwheelCarousel.min.js';
 
