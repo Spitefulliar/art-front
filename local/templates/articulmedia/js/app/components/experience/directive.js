fc92c05000a283f3533e940e1e03a407cc56eaa0
Dmitry Smirnov <pim10rentero@gmail.com>
2016-12-27 11:18:52 +0300

    fix font about block

--- local/templates/articulmedia/js/app/components/experience/directive.js
+++ local/templates/articulmedia/js/app/components/experience/directive.js
@@ -5,6 +5,30 @@ const MODULE_NAME = moduleConfig.name;
 export default ['$rootScope','$http', '$timeout', '$window', '$state', 
   function ($rootScope, $http, $timeout, $window, $state) {
     var linkFunction = function linkFunction($scope, $element, $attributes) {
+
+    	var img = $('.inner-page__bg');
+			if(img.length > 0){
+			    var offset = img.offset();
+			    function bgTransform(evt){
+			        var center_x = (offset.left) + (img.width()/4);
+			        var center_y = (offset.top) + (img.height()/4);
+			        var mouse_x = evt.pageX; var mouse_y = evt.pageY;
+			        var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+			        var degree = (radians * (360 / Math.PI) * -1) + 90; 
+			        img.css('-moz-transform', 'rotate('+degree+'deg)');
+			        img.css('-webkit-transform', 'rotate('+degree+'deg)');
+			        img.css('-o-transform', 'rotate('+degree+'deg)');
+			        img.css('-ms-transform', 'rotate('+degree+'deg)');
+			    }
+			    $('.inner-page').mousemove(bgTransform);
+			}
+
+			// $scope.$on(
+			// 	"$destroy",
+			// 	function( event ) {
+			// 		$('.inner-page').off("mousemove", bgTransform);
+			// 	}
+			// );
      
     };
   return {
