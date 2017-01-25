a1dea1ac25ee72acf89e233b5e380d85987d4091
Dmitry Smirnov <pim10rentero@gmail.com>
2017-01-25 15:01:52 +0300

    image page tmp create

---
+++ local/templates/articulmedia/js/app/pages/image/controller.js
@@ -0,0 +1,15 @@
+//CONTROLLER
+// import moduleConfig from './config';
+// const MODULE_NAME = moduleConfig.name;
+
+export default ['$scope', '$rootScope', '$location', '$log', '$timeout', '$window', '$state', '$http',
+  function($scope, $rootScope, $location, $log, $timeout, $window, $state, $http) {
+
+	$scope.imageData = { 
+  	'name': 'Lorem ipsum dolor sit amet',
+  	'position': 'Nunc semper facilisis libero vitae facilisis',
+  	'quote': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non leo eu mauris posuere feugiat. Pellentesque interdum dolor vel ipsum porttitor, eget rutrum odio',
+    'image': 'url(../local/templates/articulmedia/img/desktop/image-page-bg.jpg)'
+  };
+
+}];
