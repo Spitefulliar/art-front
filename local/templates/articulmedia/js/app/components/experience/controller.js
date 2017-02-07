00491ea59be04aa68840ba9888fe0b8115b7246d
Dmitry Smirnov <pim10rentero@gmail.com>
2017-02-07 13:02:24 +0300

    load block on exp pages

--- local/templates/articulmedia/js/app/components/experience/controller.js
+++ local/templates/articulmedia/js/app/components/experience/controller.js
@@ -6,5 +6,28 @@ export default ['$scope', '$rootScope', CONFIG.APP.PREFIX + MODULE_NAME + CONFIG
   function($scope, $rootScope, $moduleService, $location, $log, $timeout, $window, $state, $sce, $http) {
 
   $scope[CONFIG.APP.PREFIX + MODULE_NAME + CONFIG.APP.SERVICE_POSTFIX] = $moduleService;
+
+  $(document).ready(function() {
+    showElements($('.page-block-list__grid'));
+  });
+ 
+  $scope.$on(
+    "$viewContentLoaded",
+    function( event ) {
+      showElements($('.page-block-list__grid'));
+      console.log('load');
+    }
+  );
+
+
+  function showElements(element){
+    element.children('md-grid-tile').each(function(){
+      if($(this).is(':hidden')){
+        $(this).fadeIn();
+        setTimeout(function(){showElements(element)}, 500);  
+        return false;
+      }
+    });
+  }
   
 }];
--- local/templates/articulmedia/js/app/pages/industry/controller.js
+++ local/templates/articulmedia/js/app/pages/industry/controller.js
@@ -46,36 +46,4 @@ export default ['$scope', '$rootScope', '$location', '$log', '$timeout', '$windo
   	]
   };
 
-  // $(document).ready(function(){ 
-  //   var $target = $('.page-block-list__item');
-  //   var hold = 500;
-
-  //   $.each($target,function(i,t){
-  //     var $this = $(t);
-  //     setTimeout(function(){ $this.css('opacity', '1');; },i*hold);
-  //   });
-  // });
-
-  // $(document).ready(function(){ 
-  //   $scope.$on(
-  //     "$stateChangeStart",
-  //     function( event ) {
-  //       loadBlock();
-  //       console.log('load');
-  //     }
-  //   );
-  // });
-
-  // function loadBlock(){
-  //   var $target = $('.page-block-list__item');
-  //   var hold = 500;
-
-  //   $.each($target, function(i, t) {
-  //     var $this = $(t);
-  //     setTimeout(function(){ $this.css('opacity', '1'); },i*hold);
-  //   });
-  // };
-
-  
-
 }];
