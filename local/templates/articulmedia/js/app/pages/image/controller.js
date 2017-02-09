9d81f448d794835cd4817da9a08eae7a04f5f3fe
Dmitry Smirnov <pim10rentero@gmail.com>
2017-02-09 16:05:05 +0300

    image page json upd

--- local/templates/articulmedia/js/app/pages/image/controller.js
+++ local/templates/articulmedia/js/app/pages/image/controller.js
@@ -5,11 +5,19 @@
 export default ['$scope', '$rootScope', '$location', '$log', '$timeout', '$window', '$state', '$http',
   function($scope, $rootScope, $location, $log, $timeout, $window, $state, $http) {
 
-	$scope.imageData = { 
-  	'name': 'Lorem ipsum dolor sit amet',
-  	'position': 'Nunc semper facilisis libero vitae facilisis',
-  	'quote': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non leo eu mauris posuere feugiat. Pellentesque interdum dolor vel ipsum porttitor, eget rutrum odio',
-    'image': 'url(../local/templates/articulmedia/img/desktop/image-page-bg.jpg)'
-  };
+  	$scope.$on(
+	    "pageDataLoaded",
+	    function() {
+	      $scope.images = $scope.page.image.items;
+	      console.log($scope.images);
+	    }
+	  );
+
+	// $scope.imageData = { 
+ //  	'name': 'Lorem ipsum dolor sit amet',
+ //  	'position': 'Nunc semper facilisis libero vitae facilisis',
+ //  	'quote': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non leo eu mauris posuere feugiat. Pellentesque interdum dolor vel ipsum porttitor, eget rutrum odio',
+ //    'image': 'url(../local/templates/articulmedia/img/desktop/image-page-bg.jpg)'
+ //  };
 
 }];
