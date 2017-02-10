63c1772ab5e5e717131b5643997f4c7e4d537566
Dmitry Smirnov <pim10rentero@gmail.com>
2017-02-10 18:31:05 +0300

    tmp text page upd

---
+++ local/templates/articulmedia/js/app/components/pageBlockText/directive.js
@@ -0,0 +1,17 @@
+//DIRECTIVE
+import moduleConfig from './config';
+const MODULE_NAME = moduleConfig.name;
+
+export default ['$rootScope','$http', '$timeout', '$window', '$state', '$log',
+  function ($rootScope, $http, $timeout, $window, $state, $log) {
+    var linkFunction = function linkFunction($scope, $element, $attributes) {
+    };
+  return {
+    restrict: "AE",
+    link: linkFunction,
+    controller: CONFIG.APP.PREFIX + MODULE_NAME + CONFIG.APP.CONTROLLER_POSTFIX,
+    template: require('./template.html'),
+    scope: false,
+    replace: true,
+  };
+}];
