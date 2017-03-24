d0f427c5189e2f7cbf4812d8dbe66f39b37ec710
Natalia Chigryay <spiteful.liar@gmail.com>
2017-03-24 15:15:34 +0300

    menu compponent done, active states for links

--- local/templates/articulmedia/js/app/components/menu/controller.js
+++ local/templates/articulmedia/js/app/components/menu/controller.js
@@ -2,30 +2,79 @@
 import moduleConfig from './config';
 const MODULE_NAME = moduleConfig.name;
 
-export default ['$scope', '$rootScope', CONFIG.APP.PREFIX + MODULE_NAME + CONFIG.APP.SERVICE_POSTFIX, '$location', '$log', '$timeout', '$window', '$state', '$sce', '$http', '$mdSidenav','$mdMedia', 
+export default ['$scope', '$rootScope', CONFIG.APP.PREFIX + MODULE_NAME + CONFIG.APP.SERVICE_POSTFIX, '$location', '$log', '$timeout', '$window', '$state', '$sce', '$http', '$mdSidenav','$mdMedia',
   function($scope, $rootScope, $moduleService, $location, $log, $timeout, $window, $state, $sce, $http, $mdSidenav, $mdMedia) {
 
   $scope[CONFIG.APP.PREFIX + MODULE_NAME + CONFIG.APP.SERVICE_POSTFIX] = $moduleService;
 
-  //media
-  $scope.$watch(function() { return $mdMedia('gt-sm'); }, function(mquery) {
-    $scope.isDesktop = mquery;
-  });
-  $scope.$watch(function() { return $mdMedia('sm'); }, function(mquery) {
-    $scope.isTablet = mquery;
-  });
-  $scope.$watch(function() { return $mdMedia('xs'); }, function(mquery) {
-    $scope.isMobile = mquery;
-  });
+  //getting nav date fo menu
+  $scope.navData = $moduleService.getNavData();
 
+  //slider config for mobile menu
+  $scope.slickConfigMenu = {
+    enabled: true,
+    // autoplay: true,
+    draggable: true,
+    // adaptiveHeight: true,
+    autoplaySpeed: 6000,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '18%',
+    slidesToShow: 1,
+    cssEase: false,
+    useCSS: false,
+    dots: true,
+    arrows: false,
+    mobileFirst: true,
+    respondTo: 'slider',
+    swipe: true,
+    easing: 'linear',
+    swipeToSlide: true,
+    slidesToScroll: 1,
+    variableWidth: false,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          centerPadding: '30%',
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerPadding: '25%',
+        }
+      }
+    ]  
+  };
+
+  //sidenav
+  $scope.sidenavToggle = function(){
+    $mdSidenav('sidenav-right').toggle();
+  }
+
+  $scope.sidenavHide = function() {
+    $mdSidenav('sidenav-right').close();
+  }
+
+  $scope.sidenavHideMedia = function() {
+    if ($mdMedia('gt-sm')) {
+      $scope.sidenavHide();
+    }
+    return $mdMedia('gt-sm');
+  }
+
+  $scope.$watch($scope.sidenavHideMedia);
   //eof sidenav
 
-  $rootScope.$on('$stateChangeSuccess', 
+  $rootScope.$on('$stateChangeStart', 
     function(event, toState, toParams, fromState, fromParams){ 
-      $scope.pageData = $state.current.data;
+      if ($scope.sidenavIsOpen) {
+        $scope.sidenavHide();
+      }
     }
   );
-
 
   /* 
     utils 
---
+++ local/templates/articulmedia/js/app/components/menu/service.js
@@ -0,0 +1,97 @@
+//SERVICE
+export default [
+    function() {
+        let tmpNostate = "index";
+        let _menu = {
+            'menuItems': [{
+                'name': 'Компания',
+                'link': '/articul/',
+                'state': 'articul',
+                'hoverColor': 'orange',
+                'subMenu': [{
+                    'name': 'Мы – Articul',
+                    'link': '/articul/',
+                    'state': 'articul'
+                }, {
+                    'name': 'Клиенты',
+                    'link': '/clients/',
+                    'state': 'clients'
+                }, {
+                    'name': 'Команда',
+                    'link': '/crew/',
+                    'state': 'crew'
+                }, {
+                    'name': 'Articul Group',
+                    'link': '/about/',
+                    'state': 'about'
+                }]
+            }, {
+                'name': 'Экспертиза',
+                'link': '/digital/',
+                'state': 'digital',
+                'subMenu': [{
+                    'name': '360 digital',
+                    'link': '/digital/',
+                    'state': 'digital'
+                }, {
+                    'name': 'Уникальный опыт',
+                    'link': '/experience/',
+                    'state': 'experience'
+                }, {
+                    'name': 'Развивам digital-индустрию',
+                    'link': '/industry/',
+                    'state': 'industry'
+                }]
+            }, {
+                'name': 'Работы',
+                'link': '/case/',
+                'state': 'case',
+                'subMenu': [{
+                    'name': 'Все подряд',
+                    'link': '/case/',
+                    'state': tmpNostate
+                }, {
+                    'name': 'Выбрать по виду',
+                    'link': '/case/',
+                    'state': tmpNostate
+                }, {
+                    'name': 'Выбрать по бренду',
+                    'link': '/case/',
+                    'state': tmpNostate
+                }]
+            }, {
+                'name': 'Достижения',
+                'link': '/',
+                'state': tmpNostate
+            }, {
+                'name': 'Новости',
+                'link': '/news/',
+                'state': 'news'
+            }, {
+                'name': 'Карьера',
+                'link': '/jobs/',
+                'state': 'jobs'
+            }, {
+                'name': 'Контакты',
+                'link': '/',
+                'state': tmpNostate
+            }],
+            'headerLinks' : [{
+                'name': 'Articul Conceptica',
+                'link': '/',
+                'img': '/local/templates/articulmedia/img/icon/icon-с.png'
+            }, {
+                'name': 'Articul Production',
+                'link': '/',
+                'img': '/local/templates/articulmedia/img/icon/icon-p.png'
+            }]
+        };
+
+        let service = {
+            getNavData: () => {
+                return _menu;
+            }
+        };
+        return service;
+    }
+];
