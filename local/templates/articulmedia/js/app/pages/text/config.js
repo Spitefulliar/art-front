7bb72222809c6e47880f6b2aee98ea71795c39da
Dmitry Smirnov <pim10rentero@gmail.com>
2017-03-24 12:44:22 +0300

    rm old text&image tmp

--- local/templates/articulmedia/js/app/pages/text/config.js
+++
@@ -1,6 +0,0 @@
-export default  {  
-  name: 'Text',
-  title: 'Articul Media Group: digital, стратегии, креатив, SMM, мобильный маркетинг, создание и поддержка веб-проектов, мобильные приложения',
-  state: 'text',
-  url: '/text/',
-};
